3388c28829d65c4f584be2a54d7b654facdc11d7
Angel <angelchen1929@gmail.com>
2024-10-19 22:36:04 -0400

    Completed Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user's name to the given string.
+     * @param name the name of the current user
+     */
+    void setCurrentUser(String name);
 }
